c9a9b727a4babf2d136aa865d8ba1803f4a4a9c6
brianxcli <neteasy1210@gmail.com>
2020-02-10 16:04:41 +0800

    proj init & basic navigation

---
+++ AgoraLive-Android/app/src/main/java/io/agora/vlive/ui/BaseActivity.java
@@ -0,0 +1,33 @@
+package io.agora.vlive.ui;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    protected void hideStatusBar(boolean darkText) {
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+
+        int flag = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && darkText) {
+            flag = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        }
+
+        window.getDecorView().setSystemUiVisibility(flag |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+}
---
+++ AgoraLive-Android/app/src/main/java/io/agora/vlive/ui/main/MainActivity.java
@@ -0,0 +1,32 @@
+package io.agora.vlive.ui.main;
+
+import android.os.Bundle;
+
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import io.agora.vlive.R;
+import io.agora.vlive.ui.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideStatusBar(true);
+        setContentView(R.layout.activity_main);
+        initUI();
+    }
+
+    private void initUI() {
+        initNavigation();
+    }
+
+    private void initNavigation() {
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setItemIconTintList(null);
+        NavigationUI.setupWithNavController(navView,
+        Navigation.findNavController(this, R.id.nav_host_fragment));
+    }
+}
