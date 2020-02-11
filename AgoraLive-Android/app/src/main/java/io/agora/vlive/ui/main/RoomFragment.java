b82a60293463cb34a8554864ff0d6f9cb7f2d373
brianxcli <neteasy1210@gmail.com>
2020-02-11 15:08:47 +0800

    Case for no fragment action

--- AgoraLive-Android/app/src/main/java/io/agora/vlive/ui/main/RoomFragment.java
+++ AgoraLive-Android/app/src/main/java/io/agora/vlive/ui/main/RoomFragment.java
@@ -12,11 +12,13 @@ import androidx.fragment.app.Fragment;
 import io.agora.vlive.Global;
 
 public class RoomFragment extends Fragment {
+    private static final int DEFAULT_DESTINATION = Global.Constants.TAB_ID_MULTI;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Bundle bundle = getArguments();
-        int destination = bundle.getInt(Global.Constants.TAB_KEY);
+        int dest = bundle == null ? DEFAULT_DESTINATION : bundle.getInt(Global.Constants.TAB_KEY);
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
