ac8329b4dd6548a9b2425e23de4e17dce3fdc533
brianxcli <neteasy1210@gmail.com>
2020-02-11 15:12:50 +0800

    update nav menu item size

--- AgoraLive-Android/app/src/main/java/io/agora/vlive/ui/main/MainActivity.java
+++ AgoraLive-Android/app/src/main/java/io/agora/vlive/ui/main/MainActivity.java
@@ -1,10 +1,15 @@
 package io.agora.vlive.ui.main;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.lang.reflect.Field;
 
 import io.agora.vlive.R;
 import io.agora.vlive.ui.BaseActivity;
@@ -24,8 +29,26 @@ public class MainActivity extends BaseActivity {
 
     private void initNavigation() {
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        changeItemHeight(navView);
         navView.setItemIconTintList(null);
         NavigationUI.setupWithNavController(navView,
         Navigation.findNavController(this, R.id.nav_host_fragment));
+    }
+
+    private void changeItemHeight(@NonNull BottomNavigationView navView) {
+        // Bottom navigation menu uses a hardcode menu item
+        // height which cannot be changed by a layout attribute.
+        // Change the item height using reflection for
+        // a comfortable padding between icon and label.
+        int itemHeight = getResources().getDimensionPixelSize(R.dimen.nav_bar_height);
+        BottomNavigationMenuView menu =
+                (BottomNavigationMenuView) navView.getChildAt(0);
+        try {
+            Field itemHeightField = BottomNavigationMenuView.class.getDeclaredField("itemHeight");
+            itemHeightField.setAccessible(true);
+            itemHeightField.set(menu, itemHeight);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
