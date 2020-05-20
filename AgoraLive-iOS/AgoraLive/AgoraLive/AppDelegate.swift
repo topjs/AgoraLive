25c7ed8dfc009462e6ab1bb77bd175e01a89f2ff
CavanSu <403029552@qq.com>
2020-05-20 11:01:34 +0800

    light mode

--- AgoraLive-iOS/AgoraLive/AgoraLive/AppDelegate.swift
+++ AgoraLive-iOS/AgoraLive/AgoraLive/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+         if #available(iOS 13.0, *) {
+            self.window?.overrideUserInterfaceStyle = .light
+        }
+        
         let center = ALCenter.shared()
         center.appAssistant.checkMinVersion { [unowned center] in
             center.reinitAgoraServe()
